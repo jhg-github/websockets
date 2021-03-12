670a610c1a081ed09789cdece843936b5d8a1df2
javi <javi@email.com>
2021-03-12 19:14:04 +0100

    initial examples

---
+++ synchronization_example/server.py
@@ -0,0 +1,75 @@
+
+#!/usr/bin/env python
+
+# WS server example that synchronizes state across clients
+
+# A WebSocket server can receive events from clients, process them to update the application state, and synchronize the resulting state across clients.
+# Here’s an example where any client can increment or decrement a counter. Updates are propagated to all connected clients.
+# The concurrency model of asyncio guarantees that updates are serialized.
+
+
+import asyncio
+import json
+import logging
+import websockets
+
+logging.basicConfig()
+
+STATE = {"value": 0}
+
+USERS = set()
+
+
+def state_event():
+    return json.dumps({"type": "state", **STATE})
+
+
+def users_event():
+    return json.dumps({"type": "users", "count": len(USERS)})
+
+
+async def notify_state():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = state_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def notify_users():
+    if USERS:  # asyncio.wait doesn't accept an empty list
+        message = users_event()
+        await asyncio.wait([user.send(message) for user in USERS])
+
+
+async def register(websocket):
+    USERS.add(websocket)
+    await notify_users()
+
+
+async def unregister(websocket):
+    USERS.remove(websocket)
+    await notify_users()
+
+
+async def counter(websocket, path):
+    # register(websocket) sends user_event() to websocket
+    await register(websocket)
+    try:
+        await websocket.send(state_event())
+        async for message in websocket:
+            data = json.loads(message)
+            if data["action"] == "minus":
+                STATE["value"] -= 1
+                await notify_state()
+            elif data["action"] == "plus":
+                STATE["value"] += 1
+                await notify_state()
+            else:
+                logging.error("unsupported event: {}", data)
+    finally:
+        await unregister(websocket)
+
+
+start_server = websockets.serve(counter, "localhost", 6789)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
