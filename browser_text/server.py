b15ac2b1f7fe6a9614ee4bccdce9d258c9164c45
javi <javi@email.com>
2021-03-12 21:21:22 +0100

    two data send as json

--- browser_text/server.py
+++ browser_text/server.py
@@ -6,14 +6,16 @@ import asyncio
 import datetime
 import random
 import websockets
+import json
 
-async def time(websocket, path):
+async def send_data(websocket, path):
     while True:
-        data = random.random()
+        rd = random.random()
+        data = json.dumps( {"data1": str(rd), "data2": str(rd + 1)} )        
         await websocket.send( str(data) )
         await asyncio.sleep( 1 )
 
-start_server = websockets.serve(time, "127.0.0.1", 5678)
+start_server = websockets.serve(send_data, "127.0.0.1", 5678)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
